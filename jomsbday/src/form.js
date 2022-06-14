de77d4bb2c606c7a9426f25b1551c912b5a25dcd
Rohit <r24rames@uwaterloo.ca>
2022-06-14 19:11:46 -0400

    added condition after all the questions

--- jomsbday/src/form.js
+++ jomsbday/src/form.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import questions from './questions.json';
+import questions from "./questions.json";
 import Video from "./Videos.js";
 
 function Form(props) {
   const [ans, setAns] = useState("");
-  const [ansId, setAnsId] = useState(1);
+  const [ansId, setAnsId] = useState(0);
 
   const answerChangeHandler = (e) => {
     setAns(e.target.value);
@@ -12,26 +12,26 @@ function Form(props) {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log('asdasd')
-    if (questions.Questions[ansId].answer == ans) {
-      console.log('popaosda')
+    if (questions.Questions[ansId].answer === ans) {
       setAnsId(ansId + 1);
     }
   };
-
   return (
     <>
-      <label>{questions.Questions[ansId].question}</label>
-      <input
-        type="text"
-        placeholder="hit me up"
-        value={ans}
-        onChange={answerChangeHandler}
-      ></input>
-      <button onClick={submitHandler}>
-        Let's see if your answer is correct
-      </button>
-      <Video embedId={questions.Questions[ansId].link} />
+      {ansId < 4 && <>
+        <label>{questions.Questions[ansId].question}</label>
+        <input
+          type="text"
+          placeholder="hit me up"
+          value={ans}
+          onChange={answerChangeHandler}
+        ></input>
+        <button onClick={submitHandler}>
+          Let's see if you're right bitch
+        </button>
+        <Video embedId={questions.Questions[ansId].link} />
+      </>}
+      {ansId >= 4 && <Video embedId="zR43V0ogJbI" />}
     </>
   );
 }
