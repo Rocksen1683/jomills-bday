c044e98281a6096852ca3bdf8c04e4951e9a88c1
Tanmay Pilla <tpilla@uwaterloo.ca>
2022-06-14 18:12:37 -0400

    Created Videos.js and embedded a test video

--- jomsbday/src/App.js
+++ jomsbday/src/App.js
@@ -1,4 +1,6 @@
 import questions from './questions.json';
+import React from 'react';
+import Video from './Videos.js';
 import './App.css';
 import Form from './form.js';
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Jomirisu's 19th Bday</h1>
+        <h2>Here's your first one!</h2>
+        <Video embedId="rokGy0huYEA" />
         <p>
           {q.qId}
         </p>
