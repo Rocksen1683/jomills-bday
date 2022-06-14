2a2e79fd2e12ca1bcd61df73dbf5982ef07e6464
Rohit <r24rames@uwaterloo.ca>
2022-06-14 18:51:09 -0400

    modified app.js slightly and added form state functionality

--- jomsbday/src/App.js
+++ jomsbday/src/App.js
@@ -1,22 +1,14 @@
-import questions from './questions.json';
-import {CenteredForm} from './styles/stylez.js';
 import React from 'react';
-import Video from './Videos.js';
+// import './App.css';
 import Form from './form.js';
 function App() {
-
-  const q = questions.Questions[0];
 
   return (
     <div className="App">
       <header className="App-header">
-        <CenteredForm>
-          <h1>Jomirisu's 19th Bday</h1>
-          <h2>Here's your first one!</h2>
-          <Video embedId="rokGy0huYEA" />
-          <Form />
-          <p>{q.qId}</p>
-        </CenteredForm>
+        <h1>Jomirisu's 19th Bday</h1>
+        <h2>Here's your first one!</h2>
+        <Form></Form>
       </header>
     </div>
   );
--- jomsbday/src/form.js
+++ jomsbday/src/form.js
@@ -1,13 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import questions from './questions.json';
+import Video from "./Videos.js";
 
-function Form(){
+function Form(props) {
+  const [ans, setAns] = useState("");
+  const [ansId, setAnsId] = useState(1);
 
-    const [ansID, setansID] = useState(0);
+  const answerChangeHandler = (e) => {
+    setAns(e.target.value);
+  };
 
-    
-    return(<>
-    <label>Enter your answer:</label>
-    <input type="text" placeholder="hit me up"></input>
-    </>)
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log('asdasd')
+    if (questions.Questions[ansId].answer == ans) {
+      console.log('popaosda')
+      setAnsId(ansId + 1);
+    }
+  };
+
+  return (
+    <>
+      <label>{questions.Questions[ansId].question}</label>
+      <input
+        type="text"
+        placeholder="hit me up"
+        value={ans}
+        onChange={answerChangeHandler}
+      ></input>
+      <button onClick={submitHandler}>
+        Let's see if your answer is correct
+      </button>
+      <Video embedId={questions.Questions[ansId].link} />
+    </>
+  );
 }
 export default Form;
